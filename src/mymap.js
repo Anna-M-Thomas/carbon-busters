bbb054b3683148fef128e971dc6bdd8f21621674
Anna Thomas <thomasanna85@gmail.com>
2025-02-10 21:25:42 +0900

    npm update and tile update

--- src/mymap.js
+++ src/mymap.js
@@ -11,10 +11,10 @@ export const mymap = L.map(mapDiv, {
 }).setView({ lat: 38.423940781047804, lng: 141.31276130676272 }, 14);
 
 L.tileLayer(
-  "https://stamen-tiles.a.ssl.fastly.net/watercolor/{z}/{x}/{y}.jpg",
+  "https://tiles.stadiamaps.com/tiles/stamen_watercolor/{z}/{x}/{y}.jpg",
   {
     attribution:
-      "Map tiles by Stamen Design, under CC BY 3.0. Data by OpenStreetMap, under CC BY SA.",
+    "Map tiles by Stamen Design, under CC BY 3.0. Data by OpenStreetMap, under CC BY SA.",
     maxZoom: 14,
     tileSize: 512,
     zoomOffset: -1,
